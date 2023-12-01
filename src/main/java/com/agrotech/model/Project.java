0c3d6170d3273a6e0d6caa58b91ccbad94d3a11d
Luiz Meirelles <lcoutojunior@gmail.com>
2023-12-01 09:41:28 -0300

    Updating pom.xml.

--- src/main/java/com/agrotech/model/Project.java
+++ src/main/java/com/agrotech/model/Project.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 @Entity
+@Table(name = "project")
 public class ProjectModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
